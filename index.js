8bfc240d77d79d7c94813b5a8509a7eb4b708072
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-01-31 15:18:31 -0800

    implement add dept

--- index.js
+++ index.js
@@ -52,28 +52,47 @@ function init() {
 function viewAllDepartments() {
     connection.query("SELECT id as Department_ID, department_name as Department_Name from department",
         function(err, res) {
-            if (err) throw err
-            console.table(res)
-            init()
+            if (err) throw err;
+            console.table(res);
+            init();
         })
 }
 
 function viewAllRoles() {
     connection.query("SELECT roles.id as Role_ID, roles.title as Job_title, department.department_name as Department_name, roles.salary as Salary from roles JOIN department on roles.department_id = department.id",
         function(err, res) {
-            if (err) throw err
-            console.table(res)
-            init()
+            if (err) throw err;
+            console.table(res);
+            init();
         })
 }
 
 function viewAllEmployees() {
     connection.query("SELECT employee.id as Employee_ID, employee.first_name as First_Name, employee.last_name as Last_Name, roles.title as Job_title, department_name as Department,roles.salary as Salary,CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN roles on roles.id = employee.role_id INNER JOIN department on department.id = roles.department_id LEFT JOIN employee e on employee.manager_id = e.id",
         function(err, res) {
-            if (err) throw err
-            console.table(res)
-            init()
+            if (err) throw err;
+            console.table(res);
+            init();
         })
+}
+
+function addDepartment() {
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "Please enter the name of the Department?"
+    }]).then(function(res) {
+        var query = connection.query(
+            "INSERT INTO department SET ? ", {
+                department_name: res.name
+            },
+            function(err,res) {
+                if (err) throw err;
+                console.log("Department has been added");
+                init();
+            }
+        )
+    })
 }
 
 init();
